bb61db22b9304e84fbe1f2d3fb18cd6ddb0603ac
Gabriel Ramos <gcrepho@gmail.com>
2025-08-20 14:53:38 -0300

    test: refac verifications for userPage

--- bigbluebutton-tests/playwright/user/multiusers.js
+++ bigbluebutton-tests/playwright/user/multiusers.js
@@ -39,7 +39,7 @@ class MultiUsers {
     this.modPage2 = new Page(this.browser, page);
     await this.modPage2.init(true, shouldCloseAudioModal, options);
   }
-    
+
   async initUserPage(shouldCloseAudioModal = true, context = this.context, { fullName = 'Attendee', useModMeetingId = true, ...restOptions } = {}) {
     const options = {
       ...restOptions,
@@ -251,7 +251,7 @@ class MultiUsers {
     await this.userPage2.hasElement(e.isTalking, 'should display the talking indicator for the second user');
   }
 
-  async muteAllUsersExceptPresenter(){
+  async muteAllUsersExceptPresenter() {
     // join audio
     await this.modPage.joinMicrophone();
     await this.modPage2.joinMicrophone();
@@ -354,7 +354,7 @@ class MultiUsers {
   async clearAllStatusIcon() {
     await this.modPage.waitForSelector(e.whiteboard);
     await this.modPage2.waitForSelector(e.whiteboard);
-    
+
     await this.modPage.waitAndClick(e.reactionsButton);
     await this.modPage.waitAndClick(`${e.singleReactionButton}:nth-child(1)`);
     await this.modPage.hasText(e.moderatorAvatar, '😃', 'should display the smiling emoji in the moderator avatar for the moderator');
@@ -374,17 +374,15 @@ class MultiUsers {
 
   async endMeeting() {
     await this.modPage.waitForSelector(e.whiteboard);
+    await this.userPage.waitForSelector(e.whiteboard);
+
     await this.modPage.waitAndClick(e.leaveMeetingDropdown);
     await this.modPage.waitAndClick(e.endMeetingButton);
-    
-    await this.modPage.hasElement(e.simpleModal, 'should display the confirm meeting end modal');        
+    await this.modPage.hasElement(e.simpleModal, 'should display the confirm meeting end modal');
+
     await this.modPage.waitAndClick(e.confirmEndMeetingButton);
     await this.modPage.hasElement(e.meetingEndedModal, 'should display the meeting ended modal for the moderator');
-    
-    // If other users exist, verify they also see the meeting ended modal
-    if (this.userPage) {
-      await this.userPage.hasElement(e.meetingEndedModal, 'should display the meeting ended modal for the attendee');
-    }
+    await this.userPage.hasElement(e.meetingEndedModal, 'should display the meeting ended modal for the attendee');
   }
 }
 
