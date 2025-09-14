4686632e22cb1db62760b87fcea967343b06a518
Gustavo Trott <gustavo@trott.com.br>
2025-09-14 14:38:00 -0300

    Avoid blocking functions or nested lock() calls inside locked sections

--- bbb-graphql-middleware/internal/hasura/conn/writer/writer.go
+++ bbb-graphql-middleware/internal/hasura/conn/writer/writer.go
@@ -145,13 +145,7 @@ RangeLoop:
 
 							// Validate if subscription is allowed
 							if len(allowedSubscriptions) > 0 {
-								subscriptionAllowed := false
-								for _, s := range allowedSubscriptions {
-									if s == browserMessage.Payload.OperationName {
-										subscriptionAllowed = true
-										break
-									}
-								}
+								subscriptionAllowed := slices.Contains(allowedSubscriptions, browserMessage.Payload.OperationName)
 
 								if !subscriptionAllowed {
 									hc.BrowserConn.Logger.Infof("Subscription %s not allowed!", browserMessage.Payload.OperationName)
@@ -161,13 +155,7 @@ RangeLoop:
 
 							// Validate if subscription is allowed
 							if len(deniedSubscriptions) > 0 {
-								subscriptionAllowed := true
-								for _, s := range deniedSubscriptions {
-									if s == browserMessage.Payload.OperationName {
-										subscriptionAllowed = false
-										break
-									}
-								}
+								subscriptionAllowed := !slices.Contains(deniedSubscriptions, browserMessage.Payload.OperationName)
 
 								if !subscriptionAllowed {
 									hc.BrowserConn.Logger.Infof("Subscription %s not allowed!", browserMessage.Payload.OperationName)
